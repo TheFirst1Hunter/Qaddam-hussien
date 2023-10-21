e5e95124d54e8342ed8f99ee786cc190b1567b5a
TheFirst1Hunter <72226135+TheFirst1Hunter@users.noreply.github.com>
2023-10-21 22:44:06 +0300

    changes

---
+++ Scripts/Window.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+@onready var camera:Camera3D = $"Camera3D"
+@onready var glass_mesh:MeshInstance3D = $"Window/Glass"
+@onready var glass_view_port:SubViewport = $"GlassViewport"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_timer_timeout():
+	var material:Material = glass_mesh.get_active_material(0)
+	var i = glass_view_port.get_texture() 
+	print(i)
+	material.set("albedo_texture", i)
