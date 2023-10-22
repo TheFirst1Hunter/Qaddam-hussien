b54b57cac47d0f830337ffd713bf2fcad7635220
TheFirst1Hunter <72226135+TheFirst1Hunter@users.noreply.github.com>
2023-10-22 23:50:27 +0300

    stuff

--- Scripts/Window.gd
+++ Scripts/Window.gd
@@ -8,7 +8,7 @@ extends Node3D
 func _ready():
 	camera.global_transform = global_transform
 	camera.rotate_y(-90)
-	camera.transform.origin.x +=0.4
+	camera.transform.origin.x +=0.5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,8 +17,10 @@ func _process(delta):
 
 
 func _on_timer_timeout():
-	var material:Material = glass_mesh.get_active_material(0)
+	var material:Material = $Window/Glass.get_active_material(0)
 	var i = glass_view_port.get_texture() 
-	print(material)
+	$Sprite3D.texture = i
+#	print(material)
 #	$Sprite3D.texture = i
-	material.set("albedo_texture", i)
+	material.albedo_texture = i
+	material.set("albedo_texture",i)
